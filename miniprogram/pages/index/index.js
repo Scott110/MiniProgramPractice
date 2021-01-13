a13530aeb4449eab615924e30655991914bbaadb
Scott110 <heshtjs@163.com>
2021-01-13 18:38:23 +0800

    分层
    基础组件模块并跳转

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,8 +3,15 @@ const app = getApp()
 
 Page({
   data: {
-    iconType:['success','search'],
-    key:'第一个小程序'
+    funcationName_BasiceComponent:'基础组件',
 
-  }
+  },
+
+    go2ComponetMain:function(){
+      console.log('点击进入组件页面')
+        wx.navigateTo({
+          url: '../basicComponent/main/main',
+        })
+    }
+
 })
