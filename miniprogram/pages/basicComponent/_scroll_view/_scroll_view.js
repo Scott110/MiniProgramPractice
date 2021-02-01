25615b064e84c18c5d1bffc21924887f11c15f24
Scott110 <heshtjs@163.com>
2021-02-01 17:54:48 +0800

    自定义下拉刷新，recycler-view

--- miniprogram/pages/basicComponent/_scroll_view/_scroll_view.js
+++ miniprogram/pages/basicComponent/_scroll_view/_scroll_view.js
@@ -1,6 +1,13 @@
 // miniprogram/pages/basicComponent/_scroll_view/_scroll_view.js
 let viewId = 5
 
+const createRlvContext=require('miniprogram-recycle-view')
+
+function rpx2px(rpx){
+  return (rpx/750)*wx.getSystemInfoSync().windowWidth
+
+}
+
 Page({
 
   /**
@@ -9,7 +16,14 @@ Page({
   data: {
     title:"scrollView实践",
     left:0,
-    scrollIntoViewId:''
+    scrollIntoViewId:'',
+    pullMsg:'下拉刷新',//下拉刷新提示语(下拉刷新，释放刷新，刷新中)
+    //垂直滚动条位置
+    scrollTop:0,
+    //当前下拉刷新状态
+    refresherTrigger:false,
+    arr: [1,2,3,4],
+
   },
 
   /**
@@ -23,6 +37,25 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    var cxt=createRlvContext({
+      id:'recycleId',
+      dataKey:'recycleList',
+      page:this,
+      itemSize:{
+        width: rpx2px(650),
+        height: rpx2px(100)
+      }
+    })
+
+    let newList=[]
+
+    for(let i=0;i<20;i++){
+      newList.push({
+        id: i,
+    name: `标题${i + 1}`
+      })
+    }
+    cxt.append(newList)
 
   },
 
@@ -91,8 +124,42 @@ Page({
     //console.log(this.data.scrollIntoViewId)
   },
 
+  //滚动时触发
   onScroll(e){
     console.log(e.detail)
-  }
+  },
+
+  //刷新完成
+  onRefreshComplete(){
+    console.log('更新中')
+    let intervalId = setInterval(()=>{
+      let pullingMessage = this.data.pullMsg
+      console.log(pullingMessage,pullingMessage == '更新中')
+      if (pullingMessage.length < 7){
+        pullingMessage += '.'
+      }else{
+        pullingMessage = '更新中'
+      }
+      this.setData({
+        pullMsg:pullingMessage
+      })
+    },500)
+    setTimeout(()=>{
+      console.log('更新完成了')
+      clearInterval(intervalId)
+      this.setData({
+        pullMsg:"已刷新",
+        refresherTrigger:false,
+      })
+    },2000)
+  },
+
+  //向上滚动时触发
+  scrollTop(e){
+    console.log(e.detail)
+  },
+
+
+
 
 })
